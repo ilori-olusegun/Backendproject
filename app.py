e83b783bf4d2458be78f83243d134c4ac514a29b
Olusegun Ilori <80542009+ilori-olusegun@users.noreply.github.com>
2023-09-11 17:07:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,36 @@
 from flask import Flask, request, jsonify
-from datetime import datetime, timedelta
-import re
+import datetime
 
 app = Flask(__name__)
 
-@app.route('/get_info', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def get_info():
     # Get query parameters
-    Olusegun_Ilori = request.args.get('slack_name')
-    Backend = request.args.get('track')
+    slack_name = request.args.get('slack_name')
+    track = request.args.get('track')
 
-    # Calculate current day of the week and UTC time
-    current_day = datetime.utcnow().strftime('%A')
-    utc_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+    # Get current day of the week
+    current_day = datetime.datetime.utcnow().strftime('%A')
 
-    # Validate UTC time within +/-2 hours
-    utc_time_valid = validate_utc_time(utc_time)
+    # Get current UTC time
+    utc_time = datetime.datetime.utcnow().isoformat() + 'Z'
 
-    # Construct response JSON
+    # GitHub URLs
+    github_file_url = "https://github.com/ilori-olusegun/Backendproject/blob/main/app.py"
+    github_repo_url = "https://github.com/ilori-olusegun/Backendproject.git"
+
+    # Response JSON
     response = {
-      "slack_name": Olusegun_Ilori,
+        "slack_name": slack_name,
         "current_day": current_day,
         "utc_time": utc_time,
-        "track": Backend,
-        "github_file_url": "https://github.com/ilori-olusegun/Backendproject/edit/main/app.py",
-        "github_repo_url": "https://github.com/ilori-olusegun/Backendproject.git",
-        "status_code": 200 if utc_time_valid else 400  # Return 400 if UTC time is not valid
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200
     }
 
     return jsonify(response)
 
-def validate_utc_time(utc_time):
-    try:
-        current_utc_time = datetime.strptime(utc_time, '%Y-%m-%dT%H:%M:%SZ')
-        now = datetime.utcnow()
-        time_difference = abs(now - current_utc_time)
-        return time_difference <= timedelta(hours=2)
-    except ValueError:
-        return False
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
